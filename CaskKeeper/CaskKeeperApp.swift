b8101c5f623013bf28987f4ebd99fff2d325a8d9
Jon Oryhan <oryhanj@protonmail.com>
2023-11-08 20:40:13 -0800

    Initial Commit

---
+++ CaskKeeper/CaskKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaskKeeperApp.swift
+//  CaskKeeper
+//
+//  Created by Jon Oryhan on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CaskKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
