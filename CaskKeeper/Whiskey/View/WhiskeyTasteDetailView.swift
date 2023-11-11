c8f13dd659142b3eb8e92005e61a8ad3ab26104b
Jon Oryhan <oryhanj@protonmail.com>
2023-11-11 10:41:17 -0800

    Update WhiskeyTasteDetailView.swift

--- CaskKeeper/Whiskey/View/WhiskeyTasteDetailView.swift
+++ CaskKeeper/Whiskey/View/WhiskeyTasteDetailView.swift
@@ -39,6 +39,28 @@ struct WhiskeyTasteDetailView: View {
                     }
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
+                
+                VStack {
+                    Text("Score")
+                        .font(.custom("AsapCondensed-Bold", size: 20, relativeTo: .body))
+                        .foregroundStyle(.gray)
+                        .font(.headline)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.top)
+                    ZStack {
+                        Circle()
+                            .strokeBorder(Color.accentColor, lineWidth: 4)
+                            .background(Circle().fill(Color.lead))
+                            .frame(width: 200, height: 200)
+                            .shadow(color: .gray, radius: 10)
+                        Text("\(taste.score)")
+                            .font(.custom("AsapCondensed-Bold", size: 68))
+                            .foregroundColor(.accentColor)
+                    }
+                    .frame(width: 200, height: 200)
+                    .padding(.top)
+                }
+               
             }
             
             Spacer()
@@ -57,5 +79,5 @@ struct WhiskeyTasteDetailView: View {
 }
 
 #Preview {
-    WhiskeyTasteDetailView(taste: Whiskey.Taste(date: Date(), customNotes: "eqwjeqwjhidhiwhdcisbcbdwhbchdbchbdhschsbhjbcjhsbdjhbcjshdbchbsdjhbchjksbadchbsdhjbchbsd", notes: [Flavor(name: "Cherry"), Flavor(name: "Grape")]))
+    WhiskeyTasteDetailView(taste: Whiskey.Taste(date: Date(), customNotes: "eqwjeqwjhidhiwhdcisbcbdwhbchdbchbdhschsbhjbcjhsbdjhbcjshdbchbsdjhbchjksbadchbsdhjbchbsd", notes: [Flavor(name: "Cherry"), Flavor(name: "Grape")], score: 87))
 }
