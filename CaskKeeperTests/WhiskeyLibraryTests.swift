4661b0d070b445f30b62777d1bcfb3b1abb39f81
AmericanBugout <oryhanj@protonmail.com>
2023-12-05 21:46:43 -0800

    More unit tests

--- CaskKeeperTests/WhiskeyLibraryTests.swift
+++ CaskKeeperTests/WhiskeyLibraryTests.swift
@@ -34,4 +34,25 @@ final class WhiskeyLibraryTests: XCTestCase {
         whiskeyLibrary?.addWhiskey(whiskey: Whiskey(label: "testLabel", bottle: "testBottle", purchasedDate: Date(), proof: 100, bottleState: .opened, style: .bourbon, origin: .us, age: 8, price: 99.99))
         XCTAssertEqual(whiskeyLibrary?.collectionCount, 5, "Whiskey was not appended.")
     }
+    
+    func testSwipeToDeleteOffsets() {
+        let firstWhiskey = IndexSet(integer: 0)
+        whiskeyLibrary?.deleteAtIndex(index: firstWhiskey)
+        XCTAssertEqual(whiskeyLibrary?.collectionCount, 3, "Did not delete first index of array.")
+    }
+    
+    func testUpdateWhiskey() {
+        let firstWhiskey = whiskeyLibrary?.collection.first!
+        firstWhiskey?.label = "test"
+        whiskeyLibrary?.updateWhiskey(updatedWhiskey: firstWhiskey!)
+        XCTAssertEqual(whiskeyLibrary?.collection.first!.label, "test", "Update did not work.")
+    }
+    
+    func testUpdateImageData() {
+        guard let firstWhiskey = whiskeyLibrary?.collection.first! else { return }
+        let image = UIImage(named: "whiskey1")
+        guard let data = image?.pngData() else { return }
+        whiskeyLibrary?.updateImage(for: firstWhiskey, with: data)
+        XCTAssertEqual(firstWhiskey.imageData, data, "Data does not match")
+    }
 }
