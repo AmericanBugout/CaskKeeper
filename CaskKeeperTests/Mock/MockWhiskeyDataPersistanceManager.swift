abd7686d285c1d443edc57dadee45337ba6e3877
AmericanBugout <oryhanj@protonmail.com>
2023-12-07 06:53:27 -0800

    removing comments

--- CaskKeeperTests/Mock/MockWhiskeyDataPersistanceManager.swift
+++ CaskKeeperTests/Mock/MockWhiskeyDataPersistanceManager.swift
@@ -60,21 +60,4 @@ class MockWhiskeyDataPersistenceManager: WhiskeyPersisting {
     func setupLoadForFailure() {
         shouldThrowLoadError = true
     }
-    
-//    func load() -> [Whiskey] {
-//        let decoder = PropertyListDecoder()
-//        do {
-//            let data = try Data(contentsOf: WhiskeyDataPersistenceManager.collectionsFileURL)
-//            print(String(data: data, encoding: .utf8) ?? "")
-//            let collection = try decoder.decode([Whiskey].self, from: data)
-//            save(collection: collection)
-//            return collection
-//    
-//        } catch {
-//            print("Error loading data: \(error.localizedDescription)")
-//            return []
-//        }
-//    }
-    
-    
 }
